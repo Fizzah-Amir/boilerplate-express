d75820f2e6ce9eefe34f714c2817bc7f52de79c6
Fizzah-Amir <bscs24031@itu.edu.pk>
2026-03-15 15:55:36 -0700

    env

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 //console.log("Hello World");
@@ -14,8 +15,11 @@ app.use('/public',express.static(__dirname+'/public'));
 
 app.get('/json',function(req,res){
     res.json({"message":"Hello json"});
-});
 
+if(process.env.MESSAGE_STYLE==="uppercase"){
+    message=message.toUpperCase();
+}
+});
 
 
 
