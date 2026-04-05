09ff19d7c24df15c09ff92dbad8b8861987f2b16
Fizzah-Amir <bscs24031@itu.edu.pk>
2026-04-05 02:06:22 -0700

    fixed

--- myApp.js
+++ myApp.js
@@ -16,12 +16,7 @@ app.get('/',function(req,res){
 
 });
 app.use('/public',express.static(__dirname+'/public'));
-app.get('/now',function(req,res,next){
-    req.time=new Date().toString();
-    next();
-}, function(req,res){
-    res.json ({time:req.time});
-});
+
 
 app.get('/:word/echo',function(req,res){
     res.json({echo:req.params.word});
@@ -34,6 +29,9 @@ if(process.env.MESSAGE_STYLE==="uppercase"){
     message=message.toUpperCase();
 }
 res.json({"message":message});
+});
+app.get('/:word/echo',function(req,res){
+    res.json({echo:req.params.word});
 });
 
 
